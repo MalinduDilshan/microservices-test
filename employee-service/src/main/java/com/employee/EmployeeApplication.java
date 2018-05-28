a1be1613211501d9cf0cd3458b78b50d16972a8c
lakshithar <lakshithar@itx360.com>
2018-05-28 16:28:40 +0530

    Eureka Server Configuration

--- employee-service/src/main/java/com/employee/EmployeeApplication.java
+++ employee-service/src/main/java/com/employee/EmployeeApplication.java
@@ -2,10 +2,12 @@ package com.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableResourceServer
 @SpringBootApplication
+@EnableEurekaClient
 public class EmployeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApplication.class, args);
--- employee-service/src/main/java/com/employee/controller/EmployeeController.java
+++ employee-service/src/main/java/com/employee/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.employee.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/organizations")
 public class EmployeeController {
 
+    @Value("${employee.name}")
+    private String employee;
+
     @RequestMapping(value="test",method = RequestMethod.GET)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String getPage(){
-        return "Hello World";
+        return employee;
     }
 }
